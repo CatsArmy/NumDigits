40c5646ac7c80fdaf1bd1aec1ba3a23aeb1332ff
CatsArmy <104517593+CatsArmy@users.noreply.github.com>
2023-04-10 18:39:38 +0300

    Add project files.

---
+++ NumDigits/NumDigits.cs
@@ -0,0 +1,95 @@
+﻿public class NumDigits
+{
+
+    public int[] Digits;
+    private int numID;
+
+    public NumDigits(int num)
+    {
+        numID = num;
+        int maxIdx = num.ToString().Length;
+        Digits = new int[maxIdx];
+        for (int i = 0; i < maxIdx; i++)
+        {
+            Digits[i] = num % 10;
+            num /= 10;
+        }
+    }
+    public NumDigits(NumDigits otherDigit)
+    {
+        Digits = otherDigit.Digits;
+        numID = otherDigit.numID;
+    }
+    public static NumDigits[] CopyTo(NumDigits[] copy)
+    {
+        NumDigits[] to = new NumDigits[copy.Length];
+        for (int i = 0; i < to.Length; i++)
+        {
+            to[i] = new NumDigits(copy[i]);
+        }
+        return to;
+    }
+    public int GetDigit(int order)
+    {
+        if (Digits[order] > 9 || Digits[order] < 0)
+        {
+            return -1;
+        }
+        return Digits[order];
+    }
+    public static NumDigits[] BubbleSort(NumDigits[] bubble)
+    {
+        NumDigits tempBubble;
+        int tempIndx;
+        for (int i = 0; i < bubble.Length - 1; i++)
+        {
+            for (int j = 0; j < bubble.Length - 1; j++)
+            {
+                tempIndx = j + 1;
+                tempBubble = bubble[j];
+                if (bubble[tempIndx].GetNum() < bubble[j].GetNum())
+                {
+                    bubble[j] = bubble[tempIndx];
+                    bubble[tempIndx] = tempBubble;
+                }
+            }
+        }
+        return bubble;
+    }
+    public int GetNum()
+    {
+        return numID;
+    }
+    public int IncDigits(int order)
+    {
+        if (order > Digits.Length - 1)
+        {
+            order = Digits.Length -1;
+        } 
+        else if (order < 0) 
+        {
+            order = 0;
+        }
+        if (Digits[order] == 9)
+        {
+            return 0;
+        }
+        return Digits[order] + 1;
+    }
+    public int DecDigits(int order)
+    {
+        if (order > Digits.Length - 1)
+        {
+            order = Digits.Length - 1;
+        } 
+        else if (order < 0) 
+        {
+            order = 0;
+        }
+        if (Digits[order] == 0)
+        {
+            return 9;
+        }
+        return Digits[order] - 1;
+    }
+}
---
+++ NumDigits/Program.cs
@@ -0,0 +1,26 @@
+﻿Random rand = new Random();
+int num = 0;
+NumDigits[] digiArray= new NumDigits[15];
+for (int i = 0; i < digiArray.Length; i++)
+{
+    num = rand.Next(100000, 1000000);
+    NumDigits digits = new NumDigits(num);
+    digiArray[i] = digits;
+    Console.WriteLine($"Num = {num} digitArray[{i}].Digits[5] == {digits.Digits[5]}");
+}
+for (int i = 0; i < digiArray.Length; i += 2)
+{
+    num = rand.Next(1, 10);
+    if (digiArray[i].Digits.Length % 2 == 0)
+    {
+        digiArray[i].IncDigits(num);
+    }
+    else
+        digiArray[i].DecDigits(num);
+}
+NumDigits[] otherDigiArray = NumDigits.CopyTo(digiArray);
+otherDigiArray = NumDigits.BubbleSort(otherDigiArray);
+for (int i = 0; i < 15; i++)
+{
+    Console.WriteLine($"digiArray[{i}].GetNum = {digiArray[i].GetNum()} | otherDigiArray[{i}].GetNum = {otherDigiArray[i].GetNum()}");
+}
